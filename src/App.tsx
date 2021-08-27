725379793d27036dce08006bdf30d525fb7fe202
KuhniaDruggist <druggist5059@gmail.com>
2021-08-27 12:50:14 +0300

    Adds main components App and her styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import {ActionTypes, StateType} from './data/store';
 import './App.css';
+import {TableBody} from './components/TableBody/TableBody';
+import {TableHeader} from './components/TableHeader/TableHeader';
+import {Pagination} from './components/Pagination/Pagination';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+type AppPropsType = {
+    state: StateType[]
+    dispatch: (action: ActionTypes) => void
+}
+
+export const App: React.FC<AppPropsType> = ({state, dispatch}) => {
+    const [currentPage, setCurrentPage] = useState<number>(1);
+    const [rowPerPage] = useState<number>(5);
+
+    //Get current row
+    const indexOfLastRow = currentPage * rowPerPage;
+    const indexOfFirstRow = indexOfLastRow - rowPerPage;
+    const currentRow = state.slice(indexOfFirstRow, indexOfLastRow);
+
+    //Change page
+    const paginate = (pageNumber: number) => setCurrentPage(pageNumber);
+
+    return (
+        <div className="App">
+            <table className='Table'>
+                <TableHeader dispatch={dispatch}/>
+                <TableBody state={currentRow}/>
+            </table>
+            <Pagination rowPerPage={rowPerPage}
+                        totalRow={state.length}
+                        currentPage={currentPage}
+                        paginate={paginate}/>
+        </div>
+    );
 }
 
 export default App;
+
