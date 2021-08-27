cfe6fb73b1b921a1cc5e98c107d3c481bbe3b28e
KuhniaDruggist <druggist5059@gmail.com>
2021-08-27 12:49:27 +0300

    Adds function renderEntireTree and import store.

--- src/index.tsx
+++ src/index.tsx
@@ -2,16 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {store} from './data/store';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+export const renderEntireTree = () => {
+    ReactDOM.render(<App state={store.getState()} dispatch={store.dispatch.bind(store)}/>,
+        document.getElementById('root'));
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+renderEntireTree();
+
+store.subscribe(renderEntireTree);
+
